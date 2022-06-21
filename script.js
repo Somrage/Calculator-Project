7491200f3c00b63392cbe3aa47658154499883b6
Somrage <somrage@gmail.com>
2022-06-21 18:05:08 +0700

    fisrt interaction display functionality

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const display = document.querySelector('#display');
+let displayValueA
+
+const btn = document.querySelectorAll('.btn');
+let operator
+
+btn.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.className === 'btn equals') {
+      operate
+    }
+    else if (button.className === 'btn digit') {
+      display.textContent += button.textContent;
+    }
+    else if (button.className === 'btn operator') {
+      display.textContent = button.textContent;
+      operator = button.textContent;
+    }
+    else if (button.className === 'btn clear') {
+      display.textContent = '';
+    }
+  })
+});
+
+
 function add(a, b) {
     return a + b;
 }
