9d6ec45563ce617dbfb04c541345f59155f05aeb
Somrage <somrage@gmail.com>
2022-06-23 17:45:25 +0700

    update basic display value to 0

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function calculator() {
   });
 
   btnClear.addEventListener('click', () => {
-    display.textContent = '';
+    display.textContent = '0';
     a = 0
     b = 0
     operator = ''
