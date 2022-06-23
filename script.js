b3147dd056260cc7b7264bdf45ddaa1e20a95333
Somrage <somrage@gmail.com>
2022-06-23 12:46:40 +0700

    fixed addition calculations after equality, code сleaning

--- script.js
+++ script.js
@@ -1,51 +1,12 @@
 const display = document.querySelector('#display');
-const btn = document.querySelectorAll('.btn');
-let a = 0
-let b = 0
-let operator
-let result
-
-function calculator() {
-  btn.forEach(button => {
-    button.addEventListener('click', () => {
-      if (button.className === 'btn equals') {
-        b = Number(display.textContent);
-        result = operate(operator, a, b);
-        if (decimalCount(result) > 4) {
-          display.textContent = result.toFixed(4);
-        }
-        else {
-          display.textContent = result;
-        }
-      }
-      else if (button.className === 'btn digit') {
-        display.textContent += button.textContent;
-      }
-      else if (button.className === 'btn operator') {
-        a = Number(display.textContent);
-        operator = button.textContent;
-        display.textContent = '';
-      }
-      else if (button.className === 'btn clear') {
-        display.textContent = '';
-        a = 0
-        b = 0
-        operator = ''
-      }
-    })
-  });
-}
-
-//to count numbers after decimal point
-function decimalCount(number) {
-  let numStr = String(number);
-  if (numStr.includes('.')) {
-    return numStr.split('.')[1].length;
-  }
-  else {
-    return 0;
-  }
-}
+const btnDigits = document.querySelectorAll('.digit');
+const btnOperator = document.querySelectorAll('.operator');
+const btnEquals = document.querySelector('.equals');
+const btnClear = document.querySelector('.clear');
+let a = 0;
+let b = 0;
+let operator;
+let result;
 
 function add(a, b) {
     return a + b;
@@ -78,4 +39,58 @@ function operate(operator, a, b) {
     }
 }
 
-calculator()
+//to count numbers after decimal point and use toFixed
+function decimalCount(number) {
+  let numStr = String(number);
+  if (numStr.includes('.')) {
+    return numStr.split('.')[1].length;
+  }
+  else {
+    return 0;
+  }
+}
+
+function calculator() {
+  btnDigits.forEach(button => {
+    button.addEventListener('click', () => {
+      if (result !== '') {
+        display.textContent = button.textContent;
+        result = ''
+      }
+      else {
+        display.textContent += button.textContent;
+      }
+    })
+  });
+
+  btnOperator.forEach(button => {
+    button.addEventListener('click', () => {
+      a = Number(display.textContent);
+      operator = button.textContent;
+      display.textContent = '';
+    })
+  });
+
+  btnEquals.addEventListener('click', () => {
+    b = Number(display.textContent);
+    result = operate(operator, a, b);
+
+    if (decimalCount(result) > 4) {
+      display.textContent = result.toFixed(4);
+    }
+    else {
+      display.textContent = result;
+    }
+
+    a = Number(display.textContent);
+  });
+
+  btnClear.addEventListener('click', () => {
+    display.textContent = '';
+    a = 0
+    b = 0
+    operator = ''
+  });
+}
+
+calculator();
